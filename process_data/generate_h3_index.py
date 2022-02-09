a01d7951e75067a6d9db5b57c392893ab97a0c63
Niels Ehlers <niels.ehlers@gmx.net>
2022-02-09 22:54:38 +0100

    H3 Indexing

--- process_data/generate_h3_index.py
+++ process_data/generate_h3_index.py
@@ -5,8 +5,8 @@ from common import helperfunctions as helper
 import multiprocessing as mp
 print("Number of processors: ", mp.cpu_count())
 
-BATCHSIZE = 100;
-RESOLUTION = 9;
+BATCHSIZE = 5000;
+RESOLUTION = 7;
 
 TZONE = pytz.timezone(str(os.environ["TIMEZONE"]))
 
@@ -24,7 +24,9 @@ nr = 0;
 #nrTotal = result[0];
 #print("Number of units: ", nrTotal)
 
-sqlCursor.execute('SELECT ID_MASTR_UNIT, TABLENAME, LAT, LON FROM view_all_units_location WHERE id_H3 IS NULL ')
+sqlCursor.execute('SELECT ID_MASTR_UNIT, TABLENAME, LAT, LON FROM view_all_units_location ORDER BY TABLENAME, ID_MASTR_UNIT ')
+
+akt_table = "";
 while True:
     results = sqlCursor.fetchmany(BATCHSIZE)
     if not results:
@@ -37,13 +39,32 @@ while True:
 
         id_mastr = row[0]
         table    = row[1]
-        #print(str(row[2]) + "/" + str(row[3]) )
-        id_H3    = h3.geo_to_h3(float(row[2]), float(row[3]), RESOLUTION )
-        data_update.append([table,id_H3,id_mastr])
+        lat      = row[2]
+        lon      = row[3]
 
-    sqlCursor2.executemany("UPDATE ? SET id_H3 = ? WHERE id_mastr_unit = ? ",data_update)
+        if (akt_table == ""):
+            akt_table = table
+
+        if (lat is None or lon is None):
+            print("No coordinates for "+id_mastr+" ("+table+")")
+            continue
+
+
+        id_H3 = h3.geo_to_h3(float(row[2]), float(row[3]), RESOLUTION)
+        data_update.append([id_H3, id_mastr])
+
+        if (table != akt_table):
+            sql = "UPDATE "+akt_table+" SET id_H3 = ? WHERE id_mastr_unit = ? "
+            sqlCursor2.executemany(sql, data_update)
+            sqlCursor2.commit()
+            data_update = []
+            akt_table = table
+
+
+    sqlCursor2.executemany("UPDATE " + akt_table + " SET id_H3 = ? WHERE id_mastr_unit = ? ", data_update)
     sqlCursor2.commit()
 
+print("Final")
 
 
 
