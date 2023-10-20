6d08a5a48237d6d8768f6012c4b34d336edc10a5
Niels Ehlers <niels.ehlers@gmx.net>
2023-10-20 17:41:10 +0200

    Solar Histogram

--- process_data/generate_histograms.py
+++ process_data/generate_histograms.py
@@ -22,11 +22,7 @@ def generate_solar_histogram():
     sqlConnection = helper.getSqlConnection(os.environ["POWERMAP_DB"])
     sqlCursor = sqlConnection.cursor()
 
-    # Total statistics
-    with open(OUTPATH+"/histogram_solar.csv", 'w', newline='', encoding='UTF8') as f:
-        writer = csv.writer(f)
-        header = [ 'Year_Comissioning','PowerSolar_kW', 'Sum_MW', 'Number']
-        writer.writerow(header)
+    with io.open(OUTPATH + "/histogram_solar.json", 'w', encoding='utf8') as outfile:
         all_data = {}
         print("Create Histogram Solar")
         sqlstr = """SELECT  year(date_commissioning) year, 
@@ -42,9 +38,44 @@ def generate_solar_histogram():
         results = sqlCursor.fetchall()
         sqlCursor.commit()
 
-        for r in results:
-            writer.writerow(
-                    [str(r.year), r.power,  r.sum_MW, r.nr])
+        row_headers = [x[0] for x in sqlCursor.description]  # this will extract row headers
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
+        outfile.write(json.dumps(json_data, use_decimal=True, ensure_ascii=False))
+
+def generate_storage_histogram():
+
+    sqlConnection = helper.getSqlConnection(os.environ["POWERMAP_DB"])
+    sqlCursor = sqlConnection.cursor()
+
+    print("Create Histogram Storage")
+    sqlstr = """SELECT  year(date_commissioning) year, 
+                        round(power_netto) AS power, 
+                        sum(power_netto)/1000 sum_MW, 
+                        count(power_netto) nr 
+                FROM `power_units_storage` 
+                GROUP BY year,power 
+                ORDER BY year,power
+    """
+    #
+    with io.open(OUTPATH + "/histogram_storage.json", 'w', encoding='utf8') as outfile:
+
+        sqlCursor.execute(sqlstr)
+        results = sqlCursor.fetchall()
+        sqlCursor.commit()
+
+        row_headers = [x[0] for x in sqlCursor.description]  # this will extract row headers
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
+        outfile.write(json.dumps(json_data, use_decimal=True, ensure_ascii=False))
+
+
+def generate_histograms():
+    generate_solar_histogram()
+    generate_storage_histogram()
+
 
 if __name__ == "__main__":
-    generate_solar_histogram()
+    generate_histograms()
--- run_all.py
+++ run_all.py
@@ -2,11 +2,11 @@ from import_all import import_all
 from process_data.generate_json import generate_json
 from process_data.generate_kepler_data import generate_kepler_data
 from process_data.generate_h3_index import generate_h3
-from process_data.generate_histograms import generate_solar_histogram
+from process_data.generate_histograms import generate_histograms
 
 import_all()
 generate_h3()
 generate_json()
 generate_kepler_data()
-generate_solar_histogram()
+generate_histograms()
 
